680d6d0ec66165fed9a7903a44388f768848f5f9
gr434_hrdev <anna.sawhenko1984@gmail.com>
2026-02-06 17:46:36 +0700

    bug fix and complite task of add new notes

--- js/main.js
+++ js/main.js
@@ -18,17 +18,22 @@ Vue.component('notes', {
                         </div>
                         <div class="button-input">
                             <button @click="displayCreateNewNotes = false" class="create-new-notes-box exit"><p>X</p></button>
-                            <button @click="" class="create-new-notes-box add"><p>✓</p></button>
+                            <button @click="notesSave" class="create-new-notes-box add"><p>✓</p></button>
                         </div>
                     </div>
                     <div class="create-new-notes-box input-box">
-                        <input class="input-box name " type="text" placeholder="Имя заметки">
-                        <input v-for="item in inputCount" type="text" :placeholder='inputPlaceholder(item)'>
+                        <input v-model="notesName" class="input-box name " type="text" placeholder="Имя заметки">
+                        <input v-for="item in inputCount" type="text" :placeholder='inputPlaceholder(item)' v-model="notesTask[item]">
                     </div>
                 </div>
             </div>
             <div class="tabs">
-                <h2 style="color: red">Начато</h2>
+                <div><h2 style="color: red">Начато</h2>
+                <div class="card" v-for="item in notes" v-if="item.notesProcess === 'new'">
+                    <h3>{{item.name}}</h3>
+                    <div>
+                    </div>
+                </div></div>
                 <h2 style="color: orange">Еще чуть чуть</h2>
                 <h2 style="color: green">Выполнено</h2>
             </div>
@@ -42,13 +47,7 @@ Vue.component('notes', {
             displayCreateNewNotes: false,
 
             notesName: null,
-            notesTask: {
-                task1: null,
-                task2: null,
-                task3: null,
-                task4: null,
-                task5: null,
-            },
+            notesTask: ['','','','',''],
         }
     },
     methods: {
@@ -72,13 +71,41 @@ Vue.component('notes', {
                 this.errors = 'В заметке не может быть меньше 3 задач';
             }
         },
-
         notesSave() {
-            if (!this.name) {
-                this.name = 'Имя заметки'
+            let countNewNote = 0;
+            for (let item of this.notes) {
+                if (item.notesProcess === 'new') {
+                    countNewNote++;
+                }
             }
-            //вот тут продолжить
-        }
+
+            if (countNewNote < 3) {
+                if (!this.name) {
+                    this.notesName = 'Имя заметки';
+                }
+
+                let newNotes = {
+                    name: this.notesName,
+                    notes: [],
+                    notesProcess: 'new'
+                };
+
+                for (let i = 1; i < this.inputCount + 1; i++) {
+                    if (this.notesTask[i]) {
+                        newNotes.notes.push(this.notesTask[i]);
+                        this.notesTask[i] = null;
+                    } else {
+                        newNotes.notes.push('Задача без имени');
+                    }
+                }
+
+                this.notes.push(newNotes);
+                console.log(this.notes);
+                this.notesName = null;
+            } else {
+                this.errors = 'у вас больше трех начатых заметок';
+            }
+        },
     },
 })
 
